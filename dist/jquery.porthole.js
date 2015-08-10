a1fe8153315f85eacb66cc2ca87304a3d4f7a9b5
Dmitry Fillo <fillo@fillo.me>
2015-08-10 17:10:51 +0300

    compress task added to the gulpfile

--- dist/jquery.porthole.js
+++ dist/jquery.porthole.js
@@ -1,1 +1,164 @@
-!function(t,e){if("function"==typeof define&&define.amd)define(["exports","jquery"],e);else if("undefined"!=typeof exports)e(exports,require("jquery"));else{var i={exports:{}};e(i.exports,t.jQuery),t.jqueryPorthole=i.exports}}(this,function(t,e){"use strict";function i(t){return t&&t.__esModule?t:{"default":t}}function n(t,e){if(!(t instanceof e))throw new TypeError("Cannot call a class as a function")}var o=function(){function t(t,e){for(var i=0;i<e.length;i++){var n=e[i];n.enumerable=n.enumerable||!1,n.configurable=!0,"value"in n&&(n.writable=!0),Object.defineProperty(t,n.key,n)}}return function(e,i,n){return i&&t(e.prototype,i),n&&t(e,n),e}}(),s=i(e),r=function(){function t(e,i){n(this,t),this.options=e,this.viewport=i,this.init()}return o(t,[{key:"init",value:function(){"undefined"==typeof this.initialized&&(this.$viewport=s["default"](this.viewport),this.render(),this.posInit(),this.eventsBind(),this.options.callback(this),this.initialized=!0)}},{key:"destroy",value:function(){this.initialized===!0&&(this.eventsUnbind(),this.renderBack(),this.initialized=void 0)}},{key:"update",value:function(){this.destroy(),this.init()}},{key:"render",value:function(){"undefined"==typeof this.container&&(this.container=this.$viewport.attr("id")+"-porthole-wrapper",this.$container=s["default"]('<div id="'+this.container+'" style="display: inline-block;">'+this.$viewport.html()+"</div>"),this.viewportOverflow=this.$viewport.css("overflow"),this.$viewport.css("overflow","hidden").html(this.$container))}},{key:"renderBack",value:function(){"undefined"!=typeof this.container&&(this.$viewport.html(this.$container.html()).css("overflow",this.viewportOverflow),this.container=void 0,this.$container=void 0,this.viewportOverflow=void 0)}},{key:"posInit",value:function(){this.posCur=[-this.options.start[0],-this.options.start[1]],this.posMax=[this.$viewport.width()-this.$container.width(),this.$viewport.height()-this.$container.height()],this.posSet(this.posCur[0],this.posCur[1])}},{key:"posGet",value:function(){return this.$container.css("transform").split(", ").slice(-2).map(function(t){return parseInt(t)})}},{key:"posSet",value:function(t,e){t=t>0?0:t,e=e>0?0:e,t=t<this.posMax[0]?this.posMax[0]:t,e=e<this.posMax[1]?this.posMax[1]:e,this.$container.css("transform","translate3d("+t+"px, "+e+"px, 0px)")}},{key:"eventMousedown",value:function(t){var e=this;this.dragging=!0;var i=t.pageX,n=t.pageY,o=function(t){e.posSet(t.pageX-i+e.posCur[0],t.pageY-n+e.posCur[1])};$.throttle?s["default"](document).on("mousemove."+this.container,$.throttle(20,!0,function(t){o(t)})):s["default"](document).on("mousemove."+this.container,function(t){o(t)})}},{key:"eventMouseup",value:function(){this.dragging===!0&&(this.dragging=!1,this.posCur=this.posGet(),s["default"](document).off("mousemove."+this.container))}},{key:"eventsBind",value:function(){var t=this;this.$container.on("mousedown."+this.container,function(e){t.eventMousedown(e)}),s["default"](document).on("mouseup."+this.container,function(){t.eventMouseup()})}},{key:"eventsUnbind",value:function(){this.$container.off("mousedown."+this.container),s["default"](document).off("mouseup."+this.container).off("mousemove."+this.container)}}]),t}();s["default"].fn.porthole=function(t){return t=s["default"].extend({start:[0,0],callback:void 0},t),new r(t,this)}});
+(function (global, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['exports', 'jquery'], factory);
+    } else if (typeof exports !== 'undefined') {
+        factory(exports, require('jquery'));
+    } else {
+        var mod = {
+            exports: {}
+        };
+        factory(mod.exports, global.jQuery);
+        global.jqueryPorthole = mod.exports;
+    }
+})(this, function (exports, _jquery) {
+    'use strict';
+
+    var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+
+    function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+    function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+    var _jQuery = _interopRequireDefault(_jquery);
+
+    var Porthole = (function () {
+        function Porthole(options, viewport) {
+            _classCallCheck(this, Porthole);
+
+            this.options = options;
+            this.viewport = viewport;
+            this.init();
+        }
+
+        _createClass(Porthole, [{
+            key: 'init',
+            value: function init() {
+                if (typeof this.initialized === 'undefined') {
+                    this.$viewport = (0, _jQuery['default'])(this.viewport);
+                    this.render();
+                    this.posInit();
+                    this.eventsBind();
+                    this.options.callback(this);
+                    this.initialized = true;
+                };
+            }
+        }, {
+            key: 'destroy',
+            value: function destroy() {
+                if (this.initialized === true) {
+                    this.eventsUnbind();
+                    this.renderBack();
+                    this.initialized = undefined;
+                };
+            }
+        }, {
+            key: 'update',
+            value: function update() {
+                this.destroy();
+                this.init();
+            }
+        }, {
+            key: 'render',
+            value: function render() {
+                if (typeof this.container === 'undefined') {
+                    this.container = this.$viewport.attr('id') + '-porthole-wrapper';
+                    this.$container = (0, _jQuery['default'])('<div id="' + this.container + '" style="display: inline-block;">' + this.$viewport.html() + '</div>');
+                    this.viewportOverflow = this.$viewport.css('overflow');
+                    this.$viewport.css('overflow', 'hidden').html(this.$container);
+                };
+            }
+        }, {
+            key: 'renderBack',
+            value: function renderBack() {
+                if (typeof this.container !== 'undefined') {
+                    this.$viewport.html(this.$container.html()).css('overflow', this.viewportOverflow);
+                    this.container = undefined;
+                    this.$container = undefined;
+                    this.viewportOverflow = undefined;
+                };
+            }
+        }, {
+            key: 'posInit',
+            value: function posInit() {
+                this.posCur = [-this.options.start[0], -this.options.start[1]];
+                this.posMax = [this.$viewport.width() - this.$container.width(), this.$viewport.height() - this.$container.height()];
+                this.posSet(this.posCur[0], this.posCur[1]);
+            }
+        }, {
+            key: 'posGet',
+            value: function posGet() {
+                return this.$container.css('transform').split(', ').slice(-2).map(function (i) {
+                    return parseInt(i);
+                });
+            }
+        }, {
+            key: 'posSet',
+            value: function posSet(left, top) {
+                left = left > 0 ? 0 : left;
+                top = top > 0 ? 0 : top;
+                left = left < this.posMax[0] ? this.posMax[0] : left;
+                top = top < this.posMax[1] ? this.posMax[1] : top;
+                this.$container.css('transform', 'translate3d(' + left + 'px, ' + top + 'px, 0px)');
+            }
+        }, {
+            key: 'eventMousedown',
+            value: function eventMousedown(e) {
+                var _this = this;
+
+                this.dragging = true;
+                var x = e.pageX,
+                    y = e.pageY,
+                    handler = function handler(e) {
+                    _this.posSet(e.pageX - x + _this.posCur[0], e.pageY - y + _this.posCur[1]);
+                };
+                if ($.throttle) {
+                    (0, _jQuery['default'])(document).on('mousemove.' + this.container, $.throttle(20, true, function (e) {
+                        handler(e);
+                    }));
+                } else {
+                    (0, _jQuery['default'])(document).on('mousemove.' + this.container, function (e) {
+                        handler(e);
+                    });
+                }
+            }
+        }, {
+            key: 'eventMouseup',
+            value: function eventMouseup() {
+                if (this.dragging === true) {
+                    this.dragging = false;
+                    this.posCur = this.posGet();
+                    (0, _jQuery['default'])(document).off('mousemove.' + this.container);
+                }
+            }
+        }, {
+            key: 'eventsBind',
+            value: function eventsBind() {
+                var _this2 = this;
+
+                this.$container.on('mousedown.' + this.container, function (e) {
+                    _this2.eventMousedown(e);
+                });
+                (0, _jQuery['default'])(document).on('mouseup.' + this.container, function () {
+                    _this2.eventMouseup();
+                });
+            }
+        }, {
+            key: 'eventsUnbind',
+            value: function eventsUnbind() {
+                this.$container.off('mousedown.' + this.container);
+                (0, _jQuery['default'])(document).off('mouseup.' + this.container).off('mousemove.' + this.container);
+            }
+        }]);
+
+        return Porthole;
+    })();
+
+    _jQuery['default'].fn.porthole = function (options) {
+        options = _jQuery['default'].extend({
+            start: [0, 0],
+            callback: undefined
+        }, options);
+
+        return new Porthole(options, this);
+    };
+});
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 gulp.task('default', function () {
     return gulp.src('src/jquery.porthole.es6')
         .pipe(babel({
             modules : 'umd'
         }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('compress', function () {
+    return gulp.src('src/jquery.porthole.es6')
+        .pipe(babel({
+            modules : 'umd'
+        }))
         .pipe(uglify())
+        .pipe(rename('jquery.porthole.min.js'))
         .pipe(gulp.dest('dist'));
 });
