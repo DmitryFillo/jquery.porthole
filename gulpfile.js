0f9f8016ca6bf4485badfd10497297498e75d470
Dmitry Fillo <fillo@fillo.me>
2015-08-10 19:40:13 +0300

    gulp watch added

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var babel = require('gulp-babel');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var watch = require('gulp-watch');
 
 gulp.task('default', function () {
     return gulp.src('src/jquery.porthole.es6')
@@ -9,6 +10,17 @@ gulp.task('default', function () {
             modules : 'umd'
         }))
         .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('watch', function () {
+    watch('src/jquery.porthole.es6', function() {
+        return gulp.src('src/jquery.porthole.es6')
+            .pipe(babel({
+                modules : 'umd'
+            }))
+            .pipe(gulp.dest('dist'));
+    });
 });
 
 gulp.task('compress', function () {
